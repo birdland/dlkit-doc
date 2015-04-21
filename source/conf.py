2317df4f6887184a8ab1a8d13ab48ffffe9212fd
birdland <birdland@mit.edu>
2015-04-21 15:51:05 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dlkit'
-copyright = u'2013, Massachusetts Institute of Technology'
+copyright = u'2015, Massachusetts Institute of Technology, Office of Digital Learning'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
